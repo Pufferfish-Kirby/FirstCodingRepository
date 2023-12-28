160fa5e4c269082946da23c7f70e5754b814700b
Pufferfish-Kirby <68518507+Pufferfish-Kirby@users.noreply.github.com>
2023-12-28 12:42:31 -0500

    Add files via upload

---
+++ Duplicates.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+// run this through powershell if you can
+
+class Duplicates{
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("X")) {
+                break;
+            } else {
+                nums.add(Integer.parseInt(input));
+            }
+        }
+        ArrayList<Integer> cleared = new ArrayList<Integer>();
+        for (int i : nums) {
+            if (cleared.contains(i)) {
+                continue;
+            } else {
+                cleared.add(i);
+            }
+        }
+        System.out.println(cleared);
+    }
+}
